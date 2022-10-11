4eb34eefa564adc32d000829f7add10bf32f54ca
Tommy Spurlock <tspurloc@gmail.com>
2022-10-11 15:28:01 -0400

    Roommates intro

---
+++ Roommates/Roommates/Program.cs
@@ -0,0 +1,55 @@
+﻿using Roommates.Models;
+using Roommates.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace Roommates
+{
+    internal class Program
+    {
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+
+        RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+
+            Console.WriteLine("Get All Room:");
+            Console.WriteLine();
+
+            List<Room> allRooms = roomRepo.GetAll();
+
+            allRooms.ForEach(room => Console.WriteLine($"{room.Name} has an Id of {room.Id} and a max occupancy of {room.MaxOccupancy}"));
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Getting Room with Id 1");
+
+            Room singleRoom = roomRepo.GetById(1);
+
+            Console.WriteLine($"{singleRoom.Id} {singleRoom.Name} {singleRoom.MaxOccupancy}");
+
+            Room bathroom = new Room
+            {
+                Name = "Bathroom",
+                MaxOccupancy = 1
+            };
+
+            roomRepo.Insert(bathroom);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Added the new room {bathroom.Name} with an id of {bathroom.Id} ");
+
+            bathroom.MaxOccupancy = 5;
+            bathroom.Name = "Sarah's Bathroom";
+
+            roomRepo.Update(bathroom);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Changed Bathroom to {bathroom.Name}");
+
+
+            roomRepo.Delete(7);
+
+
+
+        }
+    }
+}
---
+++ Roommates/Roommates/Repositories/BaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Roommates.Repositories
+{
+    public class BaseRepository
+    {
+        /// <summary>
+        ///  A "connection string" is the address of the database.
+        /// </summary>
+        private string _connectionString;
+
+
+        /// <summary>
+        ///  This constructor will be invoked by subclasses.
+        ///  It will save the connection string for later use.
+        /// </summary>
+        public BaseRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        /// <summary>
+        ///  Represents a connection to the database.
+        ///   This is a "tunnel" to connect the application to the database.
+        ///   All communication between the application and database passes through this connection.
+        /// </summary>
+        protected SqlConnection Connection => new SqlConnection(_connectionString);
+    }
+}
